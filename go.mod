9554bc25aa4b971c085d007e7bbd727e4556e28c
nullawhale <kura98.21@gmail.com>
2021-05-06 17:32:39 +0300

    Another way to parse flags

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/fatih/color v1.10.0
+	github.com/jessevdk/go-flags v1.5.0
 	github.com/urfave/cli v1.22.5
 )
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/urfave/cli"
+	"github.com/jessevdk/go-flags"
 	"log"
 	"os"
 	"strconv"
@@ -10,102 +10,101 @@ import (
 
 var dbPath = ".db.tt"
 
-func main() {
-	app := cli.NewApp()
-	app.Name = "ttrmg"
-	app.Usage = "Tasks for cli"
-	app.Version = "0.0.1"
+var opts struct {
+	Board     string `short:"b" long:"board" description:"Specify board name" required:"false"`
+	Task      string `short:"t" long:"task" description:"Specify task name" required:"false"`
+	CheckTask string `short:"c" long:"check" description:"Check task" required:"false"`
+}
 
-	app.Action = func(c *cli.Context) error {
+func main() {
+
+	p := flags.NewParser(&opts, flags.Default)
+	if _, err := p.Parse(); err != nil {
+		if err.(*flags.Error).Type != flags.ErrHelp {
+			log.Printf("[ERROR] cli error: %v", err)
+		}
+		os.Exit(0)
+	}
+
+	/*args := []string{
+		"-n", "Me",
+	}*/
+
+	if len(os.Args) == 1 {
 		if _, err := os.Stat(dbPath); err == nil {
 			database := database{}
 			err = database.readFromDB()
 			if err != nil {
-				return err
+				fmt.Println("Unable to read db file:", err)
+				os.Exit(1)
 			}
 			database.printDB()
 		} else {
 			file, err := os.Create(dbPath)
 			if err != nil {
-				fmt.Println("Unable to create file:", err)
+				fmt.Println("Unable to create db file:", err)
 				os.Exit(1)
 			}
 			defer file.Close()
 			fmt.Println(file.Name())
 		}
-		return nil
 	}
 
-	app.Commands = []cli.Command{
-		{
-			Name:      "task",
-			ShortName: "t",
-			Usage:     "Create task",
-			Action: func(c *cli.Context) {
-				if len(c.Args()) != 2 {
-					fmt.Println()
-					fmt.Println("Error")
-					fmt.Println("You must provide a name of your task and name of board.")
-					fmt.Println("Example: ttrmg task boardName \"task text\"")
-					fmt.Println()
-					return
-				}
-
-				database := database{}
-				err := database.readFromDB()
-				if err != nil {
-					fmt.Println(err)
-				}
-
-				err = database.addTask(&task{
-					ID:     0,
-					Text:   c.Args()[1],
-					Status: false,
-				}, c.Args()[0])
-				if err != nil {
-					fmt.Println(err)
-				}
-
-				fmt.Printf("task \"%s\" is now added to your %s board.\n", c.Args()[1], c.Args()[0])
-			},
-		},
-		{
-			Name:      "check",
-			ShortName: "c",
-			Usage:     "Check task",
-			Action: func(c *cli.Context) {
-				if len(c.Args()) != 2 {
-					fmt.Println()
-					fmt.Println("Error")
-					fmt.Println("You must provide a name of your task and name of board.")
-					fmt.Println("Example: ttrmg task boardName 2")
-					fmt.Println("Example: ttrmg task boardName 5")
-					fmt.Println()
-					return
-				}
-
-				database := database{}
-				err := database.readFromDB()
-				if err != nil {
-					fmt.Println(err)
-				}
-
-				id, err := strconv.Atoi(c.Args()[1])
-				if err != nil {
-					fmt.Println(err)
-				}
-				err = database.checkTask(int64(id), c.Args()[0])
-				if err != nil {
-					fmt.Println(err)
-				}
-
-				fmt.Printf("task with id %d from board %s checked as done\n", int64(id), c.Args()[0])
-			},
-		},
-	}
-
-	err := app.Run(os.Args)
+	_, err := flags.ParseArgs(&opts, os.Args)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Unable to parse args:", err)
+		os.Exit(1)
+	}
+
+	/*fmt.Printf("Board: %s\n", opts.Board)
+	fmt.Printf("Task: %s\n", opts.Task)
+	fmt.Printf("CheckTask: %s\n", opts.CheckTask)*/
+
+	if opts.Task != "" {
+		if opts.Board == "" {
+			fmt.Println("You must provide a name of board.")
+			os.Exit(1)
+		} else {
+			database := database{}
+			err := database.readFromDB()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			err = database.addTask(&task{
+				ID:     0,
+				Text:   opts.Task,
+				Status: false,
+			}, opts.Board)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Printf("task \"%s\" is now added to your %s board.\n", opts.Task, opts.Board)
+		}
+	}
+
+	if opts.CheckTask != "" {
+		if opts.Board == "" {
+			fmt.Println("You must provide a name of board.")
+			os.Exit(1)
+		} else {
+			database := database{}
+			err := database.readFromDB()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			id, err := strconv.Atoi(opts.CheckTask)
+			if err != nil {
+				fmt.Println(err)
+			}
+			err = database.checkTask(int64(id), opts.Board)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Printf("task with id %d from board %s checked as done\n", int64(id), opts.Board)
+		}
 	}
 }
